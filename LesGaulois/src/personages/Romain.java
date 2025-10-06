cbbf56e9b6754c0237cd3a5741a369c1f1c00132
mohamed180605 <mohamed.lakhdari@univ-tlse3.fr>
2025-10-06 16:20:19 +0200

    c
    fin TP-1 GL

--- LesGaulois/src/personages/Romain.java
+++ LesGaulois/src/personages/Romain.java
@@ -1,5 +1,21 @@
 package personages;
 
 public class Romain {
-
+	private String nom ;
+	private int force ;
+	public Romain(String nom , int force ) {
+		this.nom = nom;
+		this.force = force ;
+		}
+	
+	public String getNom() {
+		return nom;
+	}
+	public void parler(String texte) {
+		System.out.println(prendreParole () + "\"" +texte + "\"");
+	}
+	private String prendreParole() {
+		return "Le romain " + nom + " : ";
+		
+	}
 }
